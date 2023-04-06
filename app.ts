7d2d91dabc804889021a17590220decd800056e8
alwin <alwin24012002@gmail.com>
2023-04-06 22:03:37 +0530

    Adding jupiter transaction

--- app.ts
+++ app.ts
@@ -12,7 +12,6 @@ import {
 import { readFileSync } from 'fs'
 import { homedir } from 'os'
 
-// connect to a cluster and get the current `slot`
 const connection = new Connection(clusterApiUrl('mainnet-beta'), 'processed')
 const slot = await connection.getSlot('recent')
 
@@ -37,7 +36,7 @@ export async function sendTransactionV0(
 	tx.sign([payer])
 	const sx = await connection.sendTransaction(tx)
 
-	console.log(`** -- Signature: https://explorer.solana.com/tx/${sx}`)
+	console.log(`Signature: https://explorer.solana.com/tx/${sx}`)
 }
 
 const payer = createKeypairFromFile(homedir() + '/second-wallet/second-keypair.json')
@@ -52,9 +51,14 @@ const [lookupTableInst, lookupTableAddress] = AddressLookupTableProgram.createLo
 
 console.log('lookup table address:', lookupTableAddress.toBase58())
 
+// transaction-1 Jupiter token to sol (performed with dev wallet)
+// transaction-2 Elixir nft buy
+// transaction-3 Elixir nft sell
+
 let txns = (
 	await connection.getTransactions(
 		[
+			'3hGG4ZRCGt6mnQJQ4Ggp2VwLiuP9ixPad5YP8F9QdcG8YTp7ay3wvN9To1vTzi5oGx6mf2sqJT8mQinzy9Uhb1ZV',
 			'4B6DemanpdnR1CgMdTyoicCHT751FoSzAYPgSaSL5iuNnG5RtACQgZ6FybKWpPM8SvHBjAr714fR3kcSw89c6K4N',
 			'4hYvajAhSJ6k9zmzMRuihrrLPuFsdKE9Am2E8VAW8dgc7PP97PiqU4Fk6ABBfsWu47T2iMwpKXsDjix9uEjpvaS8',
 		],
@@ -82,13 +86,11 @@ for (let i = 0; i < txns.length; ++i) {
 	addressesString.push(...instructions.flatMap((i) => i.keys.flatMap((k) => k.pubkey.toBase58())))
 }
 
-let existingLT = new PublicKey('3ERo1t8sjp6zUfgnsyL24wfdef6vAd54Zs7PMx74xf8g')
+// let existingLT = new PublicKey('J2Hch5H4U1UH9px6yYjfqG4an1bYNUjJDeFMtdzRwypH')
+// let existingLT = new PublicKey('6FGojTmD2qxh8t4zQMQW9b9jEkRRAt8yCR1HXY6CDxDT')
 
 //remove duplicates from addresses array
 const addresses: Array<PublicKey> = [...new Set(addressesString)].map((acc) => new PublicKey(acc))
-
-//remove addresses in existingAddresses from addresses array
-// addresses = addresses.filter((address) => !existingAddresses!.includes(address))
 
 console.log('addresses count :', addresses.length)
 
@@ -96,21 +98,27 @@ const extendInstruction1 = AddressLookupTableProgram.extendLookupTable({
 	payer: payer.publicKey,
 	authority: payer.publicKey,
 	lookupTable: lookupTableAddress,
-	addresses: addresses.slice(0, 25),
+	addresses: addresses.slice(0, 21),
 })
 
 const extendInstruction2 = AddressLookupTableProgram.extendLookupTable({
 	payer: payer.publicKey,
 	authority: payer.publicKey,
-	lookupTable: existingLT,
-	addresses: addresses.slice(25),
+	lookupTable: lookupTableAddress,
+	addresses: addresses.slice(21, 41),
 })
 
+const extendInstruction3 = AddressLookupTableProgram.extendLookupTable({
+	payer: payer.publicKey,
+	authority: payer.publicKey,
+	lookupTable: lookupTableAddress,
+	addresses: addresses.slice(41),
+})
 
 await sendTransactionV0(connection, [lookupTableInst, extendInstruction1], payer)
-//pause ececution for 10 seconds
-await new Promise((resolve) => setTimeout(resolve, 30000))
+
+await new Promise((resolve) => setTimeout(resolve, 15000))
 await sendTransactionV0(connection, [extendInstruction2], payer)
 
-
-// console.log('txnSignatures :', txnSignatures.map((i) => `https://explorer.solana.com/tx/${i}`))
+await new Promise((resolve) => setTimeout(resolve, 15000))
+await sendTransactionV0(connection, [extendInstruction3], payer)
